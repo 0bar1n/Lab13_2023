20376690b17e3f2fe5f5612c70f310de90052c2a
Баринская Ольга <114239444+barinskaya@users.noreply.github.com>
2023-03-24 13:29:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Lab_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(135, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Цвет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Толщина",
+            "2",
+            "4",
+            "6",
+            "8",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(216, 334);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 368);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "пр. 13 вариант 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        // Глобальные переменные
+        private Point PreviousPoint, point;
+        private Bitmap bmp;
+        private Pen blackPen;
+        private Graphics g;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Подготавливаем перо для рисования
+            blackPen = new Pen(Color.Black, 4);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {    OpenFileDialog dialog = new OpenFileDialog();
+            // Описываем объект класса OpenFileDialog OpenFileDialog dialog = new OpenFileDialog();
+            // Задаем расширения файлов
+            dialog.Filter = "Image files (*.BMP, *.JPG, " + "*.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+            // Вызываем диалог и проверяем выбран ли файл
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Загружаем изображение из выбранного файла
+                System.Drawing.Image image = System.Drawing.Image.FromFile(dialog.FileName); int width = image.Width;
+                int height = image.Height; pictureBox1.Width = width; pictureBox1.Height = height;
+                // Создаем и загружаем изображение в формате bmp
+                 bmp = new Bitmap(image, width, height);
+                // Записываем изображение в pictureBox1
+                pictureBox1.Image = bmp;
+                
+                // Подготавливаем объект Graphics для рисования
+                g = Graphics.FromImage(pictureBox1.Image);
+            }
+        }
+
+
+
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+ 
+            // Проверяем нажата ли левая кнопка мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                // Запоминаем текущее положение курсора мыши
+                point.X = e.X; 
+                point.Y = e.Y;
+                // Соеденяем линией предыдущую точку с текущей
+                g.DrawLine(blackPen, PreviousPoint, point);
+                // Текущее положение курсора ‐ в PreviousPoint
+                PreviousPoint.X = point.X;
+                PreviousPoint.Y = point.Y;
+                // Принудительно вызываем перерисовку
+                pictureBox1.Invalidate();
+            }
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Записываем в предыдущую точку текущие координаты
+
+            PreviousPoint.X = e.X; PreviousPoint.Y = e.Y;
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Описываем и порождаем объект
+            SaveFileDialog savedialog = new SaveFileDialog();
+            // Задаем свойства для savedialog
+            savedialog.Title = "Сохранить картинку как ..."; savedialog.OverwritePrompt = true; savedialog.CheckPathExists = true;
+            savedialog.Filter = "Bitmap File(*.bmp)|*.bmp|" + "GIF File(*.gif)|*.gif|" + "JPEG File(*.jpg)|*.jpg|" + "PNG File(*.png)|*.png";
+            // Показываем диалог и проверяем задано ли имя файла
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savedialog.FileName;
+                // Убираем из имени расширение файла
+                string strFilExtn = fileName.Remove(0,
+                fileName.Length - 3);
+                // Сохраняем файл в нужном формате
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "jpg":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif); break;
+                    case "tif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png); break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+            {
+                blackPen.Color = colorDialog1.Color;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                blackPen.Width = 2;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                blackPen.Width = 4;
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                blackPen.Width = 6;
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                blackPen.Width = 8;
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+                blackPen.Width = 10;
+            }
+        }
+
+
+    }
+}
